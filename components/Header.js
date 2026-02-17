c63b671e497987985f63b34e3653bf8579eb9168
lestday233 <email@lestday233.eu.org>
2026-02-17 17:33:25 +0800

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import { URLSearchParams } from 'node:url';
 const config = require('../wikitdb.config.js');
 
 const Header = () => {
-    const wiki = new URLSearchParams(window.location.search).get('wiki');
-    const matchedWiki = config.SUPPOST_WIKI.find(item => item.PAEAM === wiki);    
-
     return (
         <header class="relative bg-gray-800/50 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10">
             <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
